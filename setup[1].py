b75e6d138a3bd3ec49f888f4533c6700e2ddd8d3
saisreyamothe <saisreya9@gmail.com>
2026-07-03 20:11:41 +0530

    Add files via upload

---
+++ setup[1].py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="langgraph-sql-coder",
+    version="0.1.0",
+    description="Self-correcting SQL generation with LangGraph",
+    packages=find_packages(),
+)
